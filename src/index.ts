aa1e8bfbf2a9c13296e3db9ad5f30d84e03a316e
root <root@FI-CND237489T>
2023-10-31 16:02:57 +0200

    Created a test app with Elusia

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import { Elysia } from "elysia";
-import { staticPlugin } from '@elysiajs/static';
-import App from './react/App';
+import { staticPlugin } from '@elysiajs/static'
+import { renderToReadableStream } from 'react-dom/server'
+import { createElement } from "react";
+import App from './react/App'
+
+await Bun.build({
+  entrypoints: ['./src/react/index.tsx'],
+  outdir: './public',
+});
 
 const app = new Elysia()
   .use(staticPlugin())
-  .get('/', () => {
-    return 'Our first route'
+  .get('/', async () => {
+
+    // create our react App component
+    const app = createElement(App)
+
+    // render the app component to a readable stream
+    const stream = await renderToReadableStream(app, {
+      bootstrapScripts: ['/public/index.js']
+    })
+
+    // output the stream as the response
+    return new Response(stream, {
+      headers: { 'Content-Type': 'text/html' }
+    })
   })
   .listen(3000)
