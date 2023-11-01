873a62a4c14c7c8354add0efd99feb84d1da94c0
root <root@FI-CND237489T>
2023-11-01 13:11:29 +0200

    Add React Code

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,128 @@
 import { Elysia } from "elysia";
-import { staticPlugin } from '@elysiajs/static'
-import { renderToReadableStream } from 'react-dom/server'
-import { createElement } from "react";
-import App from './react/App'
+import { staticPlugin } from "@elysiajs/static";
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import cors from "cors";
+import dotenv from "dotenv";
 
-await Bun.build({
-  entrypoints: ['./src/react/index.tsx'],
-  outdir: './public',
-});
+interface UserDocument extends Document {
+  username: string;
+  password: string;
+}
+
+interface TransportationDocument extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  vehicle: string;
+  amount: number;
+  measurement: string;
+}
 
 const app = new Elysia()
   .use(staticPlugin())
-  .get('/', async () => {
+  .use(cors())
+  .listen(3000);
 
-    // create our react App component
-    const app = createElement(App)
+dotenv.config();
 
-    // render the app component to a readable stream
-    const stream = await renderToReadableStream(app, {
-      bootstrapScripts: ['/public/index.js']
-    })
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-    // output the stream as the response
-    return new Response(stream, {
-      headers: { 'Content-Type': 'text/html' }
-    })
-  })
-  .listen(3000)
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+const User = mongoose.model<UserDocument>("User", UserSchema);
+
+const TransportationSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  vehicle: String,
+  amount: Number,
+  measurement: String,
+});
+
+const Transportation = mongoose.model<TransportationDocument>(
+  "Transportation",
+  TransportationSchema
+);
+
+app.get("/", async () => {
+  // Render your React app or other content here
+});
+
+app.post("/register", async (req: any) => {
+  const { username, password } = req.body as {
+    username: string;
+    password: string;
+  };
+
+  try {
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return new Response(
+        JSON.stringify({ error: "Username already taken" }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+
+    return new Response(
+      JSON.stringify({ message: "User registered successfully" }),
+      { status: 201, headers: { "Content-Type": "application/json" } }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: "Internal server error" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+});
+
+app.post("/login", async (req: any) => {
+  const { username, password } = req.body as { username: string; password: string };
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return new Response(
+        JSON.stringify({ error: "Authentication failed" }),
+        { status: 401, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return new Response(
+        JSON.stringify({ error: "Authentication failed" }),
+        { status: 401, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    const token = jwt.sign({ username }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    return new Response(
+      JSON.stringify({ token }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: "Internal server error" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+});
--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import LoginPage, { setAuthToken } from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
 
-export default function App() {
-  const [count, setCount] = useState(0);
+function App() {
+  const loggedIn = localStorage.getItem("token");
+
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    setAuthToken(null);
+    window.location.href = "/";
+  };
   return (
-    <html>
-      <head>
-        <meta charSet="utf-8" />
-        <title>Bun, Elysia & React</title>
-        <meta name="description" content="Bun, Elysia & React" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body>
-        <h1>Counter {count}</h1>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-      </body>
-    </html>
+    <Router>
+      {loggedIn ? (
+        <button onClick={handleLogOut}>Log Out</button>
+      ) : (
+        <nav>
+          <NavLink to="/register">Register</NavLink>
+          <NavLink to="/login">Login</NavLink>
+        </nav>
+      )}
+
+      <Routes>
+        <Route path="/" element={<ProfilePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
